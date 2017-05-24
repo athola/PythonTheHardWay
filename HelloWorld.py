5b6a562640644054cbd05b656abe153bbd44cca7
Alexander Thola <alexthola@gmail.com>
2017-05-24 14:14:20 -0500

    Updated HelloWorld program.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,10 @@
-def main():
-    variable = "Hello, World!"
-    print(variable)
+print('Hello world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
 
-main()
